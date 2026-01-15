2760953ad579147077bd2acc4b5ca3700aef1dd7
Gakenye8741 <brayogakenye04@gmail.com>
2026-01-15 13:13:54 +0300

    first commit

--- src/Dashboard/DashboardDesign/AdminCard.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const AdminCard = ({ children }) => {
-    return (_jsx("div", { className: "p-3", children: children }));
-};
-export default AdminCard;
